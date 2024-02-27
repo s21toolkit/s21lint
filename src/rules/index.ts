dcb94a889ca7fc0e2e9649dda82578f3a1ecc09e
bgenia <bgenia@student.21-school.ru>
2024-02-27 21:12:15 +0300

    feat: add intendation limit rule

--- src/rules/index.ts
+++ src/rules/index.ts
@@ -1,8 +1,10 @@
 import type { Rule } from "@/rule"
 import { s21StructuralNoGoto } from "./s21-structural-no-goto"
 import { s21StructuralFunctionLineLimit } from "./s21-structural-function-line-limit"
+import { s21StructuralIndentationLimit } from "./s21-structural-indentation-limit"
 
 export const rules = [
 	s21StructuralNoGoto,
 	s21StructuralFunctionLineLimit,
+	s21StructuralIndentationLimit,
 ] satisfies Rule[]
---
+++ src/rules/s21-structural-indentation-limit.ts
@@ -0,0 +1,30 @@
+import { defineRule } from "@/rule"
+import { traverse } from "@/traversal"
+
+const INDENTATION_LIMIT = 4
+
+export const s21StructuralIndentationLimit = defineRule({
+	name: "s21-structural-indentation-limit",
+
+	check(ctx) {
+		let currentIndentation = 0
+
+		traverse(ctx.tree, {
+			compound_statement: {
+				enter(node) {
+					currentIndentation++
+
+					if (currentIndentation > INDENTATION_LIMIT) {
+						ctx.error(
+							node,
+							`code blocks must not exceed ${INDENTATION_LIMIT} indentation levels`,
+						)
+					}
+				},
+				leave() {
+					currentIndentation--
+				},
+			},
+		})
+	},
+})
