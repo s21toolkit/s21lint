2255fe6aac030641848fa35e6e058fd63a0931cb
bgenia <bgenia@student.21-school.ru>
2024-02-27 21:40:15 +0300

    feat: implement no-global-variables rule

--- src/rules/index.ts
+++ src/rules/index.ts
@@ -2,9 +2,11 @@ import type { Rule } from "@/rule"
 import { s21StructuralNoGoto } from "./s21-structural-no-goto"
 import { s21StructuralFunctionLineLimit } from "./s21-structural-function-line-limit"
 import { s21StructuralIndentationLimit } from "./s21-structural-indentation-limit"
+import { s21StructuralNoGlobalVariables } from "./s21-structural-no-global-variables"
 
 export const rules = [
 	s21StructuralNoGoto,
 	s21StructuralFunctionLineLimit,
 	s21StructuralIndentationLimit,
+	s21StructuralNoGlobalVariables,
 ] satisfies Rule[]
---
+++ src/rules/s21-structural-no-global-variables.ts
@@ -0,0 +1,28 @@
+import { defineRule } from "@/rule"
+import { oneLine } from "common-tags"
+
+const GLOBAL_VARIABLE_DECLARATION_QUERY = oneLine`
+	(translation_unit
+		(declaration
+			(type_qualifier)? @type_qualifier
+		) @declaration
+		(#not-eq? @type_qualifier "const")
+	)
+`
+
+export const s21StructuralNoGlobalVariables = defineRule({
+	name: "s21-structural-no-global-variables",
+
+	check(ctx) {
+		const globalVariableDeclarations = ctx.language
+			.query(GLOBAL_VARIABLE_DECLARATION_QUERY)
+			.captures(ctx.node)
+
+		for (const globalVariableDeclaration of globalVariableDeclarations) {
+			ctx.error(
+				globalVariableDeclaration.node,
+				"global variables are forbidden",
+			)
+		}
+	},
+})
